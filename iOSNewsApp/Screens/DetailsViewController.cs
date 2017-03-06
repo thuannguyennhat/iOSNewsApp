0501d46d956aca80830077bbddf15b826daec07c
thuannguyennhat <ngnhatthuan@gmail.com>
2017-03-06 09:53:11 +0700

    [iOS] Create ViewControllder

---
+++ iOSNewsApp/Screens/DetailsViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace iOSNewsApp
+{
+	public partial class DetailsViewController : UIViewController
+	{
+		public DetailsViewController() : base("DetailsViewController", null)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
